85a7765e9b3be4da63a404f1f9db3d461700e0d7
Bartosz Malolepszy <bartosz.malolepszy@interia.com>
2023-09-25 21:41:59 +0200

    fix 8 module, accorion, price, images

--- dist/js/script.js
+++ dist/js/script.js
@@ -102,6 +102,10 @@
         select.menuProduct.priceElem
       );
 
+      thisProduct.imageWrapper = thisProduct.element.querySelector(
+        select.menuProduct.imageWrapper
+      );
+
       console.log(this.getElements);
     }
 
@@ -121,20 +125,17 @@
 
         /* find active product (product that has active class) */
 
-        const activeProducts = document.querySelectorAll(
+        const activeProduct = document.querySelector(
           select.all.menuProductsActive
         );
 
-        console.log(activeProducts);
+        console.log(activeProduct);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        for (let activeProduct of activeProducts) {
-          if (
-            activeProduct !== thisProduct.element 
-          ) {
-            activeProduct.classList.remove('active');
-            console.log(activeProduct);
-          }
+        if (activeProduct !== null && activeProduct !== thisProduct.element) {
+          activeProduct.classList.remove('active');
+          console.log(activeProduct);
         }
+
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
       });
@@ -161,7 +162,6 @@
     }
 
     processOrder() {
-
       const thisProduct = this;
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
@@ -169,35 +169,55 @@
 
       // set price to default price
       let price = thisProduct.data.price;
+      console.log(price);
 
       // for every category (param)
-      for(let paramId in thisProduct.data.params) {
+      for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
 
         // for every option in this category
-        for(let optionId in param.options) {
+        for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
 
-        // check if there is param with a name of paramId in formData and if it includes optionId
-        if(formData[paramId] && formData[paramId].includes(optionId)) {
-          // check if the option is not default
-          if(option.default == true) {
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          const optionSelected =
+            formData[paramId] && formData[paramId].includes(optionId);
 
-            // add option price to price variable (actual)
+          if (optionSelected) {
+            // check if the option is not default
+            if (!option.default) {
+              // add option price to price variable (actual)
 
-            price = price + option.price;
-
+              price += option.price;
+            }
+          } else {
+            // check if the option is default
+            if (option.default) {
+              // reduce price variable
+              price -= option.price;
+            }
           }
-        } else {
-          // check if the option is default
-          if(option.default !==  true) {
-            // reduce price variable
-            price = price - option.price;
-          }
-        }
 
+          // Images starting
+          const optionImage = thisProduct.imageWrapper.querySelector(
+            '.' + paramId + '-' + optionId
+          ); // '.' we are looking after class
+          console.log(optionImage);
+
+          // check if the option exists
+          if (optionImage) {
+            if (optionSelected) {
+              // if the option exists add class active
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              // remove class active
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
+
+          // Images end
         }
       }
 
